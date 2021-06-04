1db8a8e12c9ba445f2518699a31e5f7d076e16eb
cryptic-exe <dmor83@gmail.com>
2021-06-04 21:52:48 +0530

    linear search added

---
+++ Array/linear search.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int linearSearch(int arr[],int n,int key){
+    for (int i = 0; i < n;i++){
+        if(key==arr[i])
+            return i;
+    }
+    return -1;
+}
+
+int main(){
+    int n, key;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n;i++){
+        cin >> arr[i];
+    }
+    cin >> key;
+    cout << linearSearch(arr, n, key) << endl;
+    return 0;
+}
