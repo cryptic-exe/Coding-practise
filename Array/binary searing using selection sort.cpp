5299a5abaa62f5e76a00201376987a40b8efeee8
cryptic-exe <dmor83@gmail.com>
2021-06-05 23:54:09 +0530

    corrected selection sorting algo

--- Array/binary searing using selection sort.cpp
+++ Array/binary searing using selection sort.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 
 void SelectionSort(int arr[],int n){
-    int min_index;
+    
     for (int i = 0; i < n - 1; i++)
     {
-        min_index = i;
-        for (int j = i + 1; j < n;j++){
-            if(arr[j]<arr[min_index]){
-                min_index = j;
+        int loc = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if(arr[]>arr[j]){
+                loc = j;
+                min = arr[j];
             }
-            swap(arr[min_index],arr[i]);
+            swap(arr[loc],arr[i]);
         }
     }
     for (int i = 0; i < n; i++){
