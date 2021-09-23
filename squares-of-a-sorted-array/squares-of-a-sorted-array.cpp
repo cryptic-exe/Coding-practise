871319f22d9612da39df7a8cef417729b2b6cc36
Cryptic <37707399+cryptic-exe@users.noreply.github.com>
2021-09-23 15:09:54 +0530

    Time: 20 ms (98.35%), Space: 26 MB (34.70%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            nums[i]=nums[i]*nums[i];
+        int left=0,right=nums.size()-1,index = nums.size()-1;
+        vector<int> ans(nums.size());
+        while(left<=right){
+            int val1 = nums[left]*nums[left];
+            int val2 = nums[right]*nums[right];            
+            if(val1<val2){
+                ans[index] = val2;
+                right--;
+           
+             }
+            else{
+                ans[index] = val1;
+                left++;
+                
+            }
+               index--; 
         }
-        sort(nums.begin(),nums.end());
-        return nums;
+    return ans;
     }
 };
