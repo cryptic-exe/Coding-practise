0a69a379fe6cd6b6bc483f1c597fa6286879b6f2
Cryptic <37707399+cryptic-exe@users.noreply.github.com>
2021-09-30 18:07:10 +0530

    Time: 32 ms (64.73%), Space: 89.6 MB (37.58%) - LeetHub

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,11 +3,9 @@ public:
     double findMedianSortedArrays(vector<int>& v1, vector<int>& v2) {
         v1.insert(v1.end(),v2.begin(),v2.end());
         sort(v1.begin(),v1.end());
-        double result;
         int n = v1.size()-1;
         if(n%2!=0) {
-            result  = (v1[n/2]+v1[(n/2)+1])/2.0;
-            return result;
+            return (v1[n/2]+v1[(n/2)+1])/2.0;
         }
         else{
             return v1[n/2];
